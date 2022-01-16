06793648ccf6c916533b501e04164fba22c9d683
fordreaming1@yeah.net <fordreaming1@yeah.net>
2022-01-16 20:11:43 +0800

    apue exams

---
+++ Apue3EQt/Apue3EQt.pro
@@ -0,0 +1,5 @@
+TEMPLATE = subdirs
+
+SUBDIRS += \
+    charpter1 \
+    charpter7
---
+++ Apue3EQt/charpter1/main.cpp
@@ -0,0 +1,8 @@
+//#include <QCoreApplication>
+#include "ls1.c"
+
+int main(int argc, char *argv[])
+{
+    main_ls(argc, argv);
+}
+
